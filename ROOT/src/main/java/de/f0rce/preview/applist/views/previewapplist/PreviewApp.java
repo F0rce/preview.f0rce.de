7c051c7f2628a0741656ac297bb3e7b5b044d611
David Dodlek <david@dodlek.com>
2021-08-20 17:51:25 +0200

    v1.0.0

---
+++ ROOT/src/main/java/de/f0rce/preview/applist/views/previewapplist/PreviewApp.java
@@ -0,0 +1,53 @@
+package de.f0rce.preview.applist.views.previewapplist;
+
+public class PreviewApp {
+
+	private String image;
+	private String name;
+	private String version;
+	private String post;
+	private String url;
+
+	public PreviewApp() {
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getPost() {
+		return post;
+	}
+
+	public void setPost(String post) {
+		this.post = post;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
---
+++ ROOT/src/main/java/de/f0rce/preview/applist/views/previewapplist/PreviewAppListView.java
@@ -0,0 +1,97 @@
+package de.f0rce.preview.applist.views.previewapplist;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@PageTitle("Preview App List")
+@Route(value = "")
+@Theme(themeFolder = "previewapplist", variant = Lumo.DARK)
+public class PreviewAppListView extends Div implements AfterNavigationObserver {
+
+	Grid<PreviewApp> grid = new Grid<>();
+
+	public PreviewAppListView() {
+		addClassName("preview-app-list-view");
+		setSizeFull();
+		grid.setHeight("100%");
+		grid.addThemeVariants(GridVariant.LUMO_NO_BORDER, GridVariant.LUMO_NO_ROW_BORDERS);
+		grid.addComponentColumn(app -> createCard(app));
+		add(grid);
+	}
+
+	private HorizontalLayout createCard(PreviewApp previewApp) {
+		HorizontalLayout card = new HorizontalLayout();
+		card.addClassName("card");
+		card.setSpacing(false);
+		card.getThemeList().add("spacing-s");
+
+		Image image = new Image();
+		image.setSrc(previewApp.getImage());
+		VerticalLayout description = new VerticalLayout();
+		description.addClassName("description");
+		description.setSpacing(false);
+		description.setPadding(false);
+
+		HorizontalLayout header = new HorizontalLayout();
+		header.addClassName("header");
+		header.setSpacing(false);
+		header.getThemeList().add("spacing-s");
+
+		Span name = new Span(previewApp.getName());
+		name.addClassName("name");
+		Span date = new Span(previewApp.getVersion());
+		date.addClassName("version");
+		header.add(name, date);
+
+		Span post = new Span(previewApp.getPost());
+		post.addClassName("post");
+
+		description.add(header, post);
+		card.add(image, description);
+		return card;
+	}
+
+	@Override
+	public void afterNavigation(AfterNavigationEvent event) {
+
+		// Set some data when this view is displayed.
+		List<PreviewApp> persons = Arrays.asList( //
+				createApp(
+						"https://static.vaadin.com/directory/user402520/icon/file6703307351609158308_1617024863123file456055440023195234_1579352673914ace-field-widget-screenshot-1_0.png",
+						"Ace", "1.3.2", "Fast and Lightweight Ace Editor (https://ace.c9.io/) for Vaadin 14+",
+						"https://preview.f0rce.de/ace"));
+
+		grid.setItems(persons);
+
+		grid.addItemClickListener(e -> {
+			UI.getCurrent().getPage().setLocation(e.getItem().getUrl());
+		});
+	}
+
+	private static PreviewApp createApp(String image, String name, String version, String post, String url) {
+		PreviewApp app = new PreviewApp();
+		app.setImage(image);
+		app.setName(name);
+		app.setVersion(version);
+		app.setPost(post);
+		app.setUrl(url);
+
+		return app;
+	}
+
+}
